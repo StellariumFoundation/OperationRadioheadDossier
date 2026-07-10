4d64087aac1bd15b7b6aca6042c0cefa083ef602
John Victor <john.victor.crown@gmail.com>
2026-07-10 05:49:47 -0300

    ok

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.operationradiohead.app',
-  appName: 'Operation Radiohead',
-  webDir: 'dist',
-};
-
-export default config;
